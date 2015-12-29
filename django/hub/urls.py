932dbc856e03f829a9f23f133ddf257162e5750b
jennyzdse <jenny.zdse@gmail.com>
2015-12-29 15:41:33 +0100

    Added index html page

---
+++ django/hub/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+#    url(r'^book/(?P<id>[0-9]+)/$', views.book, name='book'),
+#    url(r'^review/(?P<id>[0-9]+)/$', views.review, name='review'),
+#    url(r'^newbook/$', views.newbook, name='newbook'),
+#    url(r'^about/$', views.about, name='about'),
+#    url(r'^login/$', views.login, name='login'),
+#    url(r'^logout/$', views.logoutview, name='logoutview'),
+#    url(r'^search/$', views.search, name='search'),
+#    url(r'^search_result/(?P<q>.+)/$', views.search_result, name='search_result'),
+#    url(r'^(?P<book_id>[0-9]+)/newreview/$', views.newreview, name='newreview'),
+]
--- django/hub/views.py
+++ django/hub/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from models import *
 
-# Create your views here.
+
+def index(request):
+    active_project_list = Project.objects.order_by('name')[:5]
+    active_author_list = Author.objects.order_by('name')[:5]
+    context = {
+          'active_project_list': active_project_list,
+          'active_author_list': active_author_list
+          }
+    return render(request, 'hub/index.html', context)
+
+def search(request):
+    q = request.POST['query']
+    return HttpResponseRedirect(reverse('search_result', args=(q,)))
+
