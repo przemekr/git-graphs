962602781ef414af765a13e51c4eb7dad535cae7
Przemyslaw Rzepecki <przemyslaw.rzepecki@ericsson.com>
2016-01-08 10:21:41 +0100

    Added tooltips for the Author view graphs

--- gitlog_to_csv.py
+++ gitlog_to_csv.py
@@ -66,19 +66,25 @@ def main(argv):
     for url, branch, handler.repoName, query in repos:
         d = "workdir/"+handler.repoName
         if not os.path.exists(d):
+           # A new repository, clone it, then fetch just to set the FETCH_HEAD pointer.
            os.makedirs(d)
            os.popen("cd %s && git clone --bare %s .git" % (d, url)).read()
+           os.popen("cd %s && git fetch %s" % (d, url)).read()
            prev_head = ""
         else:
+           # Existing repo, check the HEAD pointer, it should be the last known commit.
            prev_head = os.popen("cd %s && git log -1 --format=%%H" % d).read().strip() + ".."
            print prev_head
            os.popen("cd %s && git fetch %s" % (d, url)).read()
 
-        log = os.popen("cd %s && git log %s --date=iso --stat %s"% (d, prev_head, query)).read();
+        log = os.popen("cd %s && git log %sFETCH_HEAD --date=iso --stat %s"% (d, prev_head, query)).read();
         entries = log.split("commit ")
         entries = map(entry, entries)
         entries = filter(lambda x:x, entries)
         map(handler, entries)
+
+        # All went OK. Update the HEAD pointer in the repo.
+        os.popen("cd %s && cp .git/FETCH_HEAD .git/HEAD "% d).read();
 
 if __name__ == '__main__':
    main(sys.argv)
